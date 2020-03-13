1f6b8fba98268bfdf07bf4e826cd72717075a3ed
Nicholas Finch <nicfinch223@gmail.com>
2020-03-12 22:20:18 -0400

    app crashes after first question

--- app.js
+++ app.js
@@ -4,32 +4,165 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
+const theTeam = [];
+
+const employeeType = [
+    {
+        type: 'list',
+        name: 'employee',
+        message: 'Select a type of Employee',
+        default: 'intern',
+        choices: [
+            'intern',
+            'engineer',
+            'manager'
+        ]
+    }
+]
+
+const internQ = [
+    {
+         type: 'input',
+         name: 'name',
+         message: "What is the Intern's name?"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the Intern's email?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter an Employee id #'
+
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What school did the Intern attend?'
+    }
+]
+
+const engineerQ = [
+    {
+         type: 'input',
+         name: 'name',
+         message: "What is the Engineer's name?"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the Engineer's email?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter an Employee id #'
+
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?'
+    }
+]
+
+const managerQ = [
+    {
+         type: 'input',
+         name: 'name',
+         message: "What is the Manager's name?"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the Manager's email?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter an Employee id #'
+
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is the Manager's office number"
+    }
+]
+
+const continueQ = [
+    {
+        type: 'list',
+        name: 'continue',
+        message: 'Would you like to add another employee?',
+        choices: [
+            'yes',
+            'no'
+        ]
+    }
+]
+
+const continuePrompt = () => {
+    inquirer.prompt(continueQ).then(function(data) {
+        if (data.continue === 'yes') {
+            getInfo()
+        } else {
+            writeFile(theTeam);
+        }
+    })
+}
+
+const interPrompt = () => {
+    inquirer.prompt(internQ).then(function(data) {
+        let intern = new Intern(data.name, data.email, data.id, data.school);
+        theTeam.push(intern);
+        continuePrompt();
+    })
+}
+
+const engineerPrompt = () => {
+    inquirer.prompt(engineerQ).then(function(data) {
+        let engineer = new Engineer(data.name, data.email, data.id, data.github);
+        theTeam.push(engineer);
+        continuePrompt();
+    })
+}
+
+const managerPrompt = () => {
+    inquirer.prompt(managerQ).then(function(data) {
+        let manager = new Manager(data.name, data.email, data.id, data.office);
+        theTeam.push(manager);
+        continuePrompt();
+    })
+}
+
+const getInfo = () => {
+    inquirer.prompt(employeeType).then(function(data) {
+        if (data.employeeType === 'intern') {
+            interPrompt();
+        } else if (data.employeeType === 'engineer') {
+            engineerPrompt();
+        } else if (data.employeeType === 'manager') {
+            managerPrompt();
+        }
+    })
+}
+
+const writeFile = (data) => {
+    fs.writeFile('./output/team.html', render(data), (error) => {
+        if (error) {
+            throw error;
+        }
+        console.log('Page created!!');
+    })
+}
+
+getInfo();
+
+
